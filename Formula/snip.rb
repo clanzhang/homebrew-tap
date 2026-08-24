732c057a1376e4e98b768259b40a737f6ea26f28
cian.z <hi_leo@yeah.net>
2026-08-24 15:14:00 +0800

    snip: bump to v0.1.6

--- Formula/snip.rb
+++ Formula/snip.rb
@@ -3,8 +3,8 @@
 class Snip < Formula
   desc "macOS clipboard and keyboard diagnostic tool"
   homepage "https://github.com/clanzhang/snip"
-  url "https://github.com/clanzhang/snip/archive/refs/tags/v0.1.5.tar.gz"
-  sha256 "15200737832b7b359b62ff31f5857703b06f37e09c22e471cc1353daa5c7df01"
+  url "https://github.com/clanzhang/snip/archive/refs/tags/v0.1.6.tar.gz"
+  sha256 "8d93a063a5f2bd42230218c14c4718971cf81f085780b20a028b41aaab8e4a13"
   license "MIT"
 
   depends_on :macos
